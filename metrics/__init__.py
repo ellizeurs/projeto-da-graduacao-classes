a81b83d7746c1995cdfde1f492149f26f5116f95
Ellizeu Sena <ellizeurs@gmail.com>
2024-07-24 16:22:37 -0300

    Update metrics

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -3,3 +3,7 @@ from darts.metrics import mape, mse, rmse
 from .hurst import Hurst
 from .utheil import UTheil
 from .sle import sle
+
+from sklearn.metrics import mean_squared_error as mse
+from sklearn.metrics import mean_absolute_percentage_error as mape
+from sklearn.metrics import root_mean_squared_error as rmse
