4c6da71162d4759753bdb5481acc9a2dd57b6fc8
Ellizeu Sena <ellizeurs@gmail.com>
2024-08-22 15:09:24 -0300

    Test imp

--- models/imp.py
+++ models/imp.py
@@ -5,6 +5,66 @@ from .torch_generic_model import TorchGenericModel
 from .window.sliding_window import SlidingWindow
 
 
+class MorphologicalLayer(torch.nn.Module):
+    def __init__(self, input_size, output_size):
+        super(MorphologicalLayer, self).__init__()
+        self.dilation_weight = torch.nn.Parameter(torch.randn(output_size, input_size))
+        self.erosion_weight = torch.nn.Parameter(torch.randn(output_size, input_size))
+
+    def forward(self, x):
+        dilation = torch.max(x.unsqueeze(1) + self.dilation_weight, dim=2)[0]
+        erosion = torch.min(x.unsqueeze(1) - self.erosion_weight, dim=2)[0]
+        output = dilation + erosion
+        return output
+
+
+class IMP(TorchGenericModel):
+    def __init__(
+        self,
+        input_chunk_length,
+        output_chunk_length,
+        batch_size,
+        lr=1e-3,
+        n_epochs=100,
+        random_state=None,
+        loss_fn=torch.nn.MSELoss(),
+        optimizer_cls=torch.optim.Adam,
+        window_model=SlidingWindow(),
+        preprocessing=None,
+        pl_trainer_kwargs=None,
+    ):
+        super().__init__(
+            input_chunk_length,
+            output_chunk_length,
+            batch_size,
+            loss_fn,
+            n_epochs,
+            random_state,
+            window_model,
+            preprocessing,
+            pl_trainer_kwargs,
+        )
+
+        self.morph_layer = MorphologicalLayer(input_chunk_length, output_chunk_length)
+        self.optimizer = optimizer_cls(self.parameters(), lr=lr)
+
+    def forward(self, x):
+        return self.morph_layer(x)
+
+    def incremental_update(self, x, y, learning_rate=0.01):
+        output = self.forward(x)
+        loss = self.loss_fn(output, y)
+        loss.backward()
+        with torch.no_grad():
+            for param in self.parameters():
+                param -= learning_rate * param.grad
+        self.zero_grad()
+
+    def configure_optimizers(self):
+        return self.optimizer
+
+
+"""""
 class IMP(TorchGenericModel):
     def __init__(
         self,
@@ -57,3 +117,4 @@ class IMP(TorchGenericModel):
         out = self.morph_layer(out)
         out = self.output_layer(out)
         return out
+""" ""
